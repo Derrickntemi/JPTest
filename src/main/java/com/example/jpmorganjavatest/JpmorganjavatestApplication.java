947fe39e60ded267b2c7689d0ac98e577ec88ce5
Derrick Kimathi Ntemi <derrick.ntemi@free-now.com>
2022-02-19 16:25:23 +0100

    initial set up

---
+++ src/main/java/com/example/jpmorganjavatest/JpmorganjavatestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpmorganjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmorganjavatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpmorganjavatestApplication.class, args);
+	}
+
+}
